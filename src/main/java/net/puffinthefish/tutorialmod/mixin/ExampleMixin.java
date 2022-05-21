b3a73b322b82e61818dd642ba3eff309b6c71f76
Dane Edwards <dane.edwards2605@gmail.com>
2022-05-22 11:48:18 +1200

    Initial commit

---
+++ src/main/java/net/puffinthefish/tutorialmod/mixin/ExampleMixin.java
@@ -0,0 +1,18 @@
+package net.puffinthefish.tutorialmod.mixin;
+
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.puffinthefish.tutorialmod.TutorialMod;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+
+		TutorialMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
