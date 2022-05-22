0fb16bfbf1317f59edbb19e7cff2e2e884f80859
Dane Edwards <dane.edwards2605@gmail.com>
2022-05-22 12:18:02 +1200

    added an item

--- src/main/java/net/puffinthefish/tutorialmod/item/ModItems.java
+++ src/main/java/net/puffinthefish/tutorialmod/item/ModItems.java
@@ -1,4 +1,25 @@
 package net.puffinthefish.tutorialmod.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.puffinthefish.tutorialmod.TutorialMod;
+
 public class ModItems {
+    public static final Item ALTIUM_SHARD =  registerItem("altium_shard",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+
 }
